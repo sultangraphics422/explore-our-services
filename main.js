b6b35f114c2d28c57523f866f8354b49d3e90d36
Muhammad Imran <112778411+sultangraphics422@users.noreply.github.com>
2024-12-03 10:48:34 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".service-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1024: {
+                items: 3
+            }
+        }
+    });
+});
